b8e8eac0b7e704f9b2f1bc2386b6230dfc14c3df
AngesPhilippe <angesPhilippe@users.noreply.github.com>
2020-12-13 21:38:19 +0100

    first commit

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategorieController extends AbstractController
+{
+    /**
+     * @Route("/categorie/show/{sms?}", name="categorie_show", methods={"POST","GET"})
+     */
+    public function index(): Response
+    {
+        return $this->render('categorie/index.html.twig', [
+            'controller_name' => 'CategorieController',
+        ]);
+    }
+}
